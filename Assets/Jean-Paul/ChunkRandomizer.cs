52aec60d6eccb7ea6ba284a82dd14a91c39ccce5
Jean-Paul <35977754+imurhero2@users.noreply.github.com>
2019-11-09 17:51:56 -0500

    Procedural Obstacle Generation + Anima2D Import

---
+++ Assets/Jean-Paul/ChunkRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkRandomizer : MonoBehaviour
+{
+	public List<GameObject> obstaclePrefabs;
+	public List<GameObject> obstacleHotspots;
+
+	void Start()
+    {
+		foreach (GameObject hotspot in obstacleHotspots)
+		{
+			if (Random.Range(0, 100) > 50)
+			{
+				Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)], hotspot.transform.position, Quaternion.identity);
+			}
+		}
+    }
+}
