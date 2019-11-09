17aa4fd48edf8c1623038204d5350fe7e8542ac0
Jean-Paul <35977754+imurhero2@users.noreply.github.com>
2019-11-09 12:38:46 -0500

    Player Movement + Camera Follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public float followSpeed = 2f;
+	public Transform followTarget;
+
+    void FixedUpdate()
+    {
+		Vector3 newPosition = followTarget.position;
+		newPosition.z = -10;
+		transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float movementSpeed;
+
+	private void Start()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		transform.Translate(Vector2.right * Time.deltaTime * movementSpeed);
+	}
+}
