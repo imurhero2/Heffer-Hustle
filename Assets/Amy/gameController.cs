0d4136f9806df0bf2346fe4d5bd940e1643ab77d
amySchwingeDM <54595377+amySchwingeDM@users.noreply.github.com>
2019-11-09 13:09:04 -0500

    Update gameController.cs

--- Assets/Amy/gameController.cs
+++ Assets/Amy/gameController.cs
@@ -1,22 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class gameController : MonoBehaviour
+public class DestroyByContact : MonoBehaviour // Code to destroy "Money Bags" and tell the Game Controller script to increase the player's score
 {
-    public Text score;
-
-    private int scoreValue = 0;
+    public int scoreValue;
+    private gameController gameController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<gameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'gameController' script");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider player)
+    {
+        gameController.AddScore(scoreValue);
+        Destroy(gameObject);
     }
 }
