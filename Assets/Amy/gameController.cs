9a1d19595f65401a20bbb8ac30d1566827f23635
amySchwingeDM <54595377+amySchwingeDM@users.noreply.github.com>
2019-11-10 10:15:11 -0500

    Add files via upload

--- Assets/Amy/gameController.cs
+++ Assets/Amy/gameController.cs
@@ -5,18 +5,31 @@ using UnityEngine.UI;
 
 public class gameController : MonoBehaviour
 {
-    public Text score;
+    public GameObject obstacle;
+    public Text ScoreText;
 
-    private int scoreValue = 0;
+    private int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        UpdateScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Score: " + score;
     }
 }
